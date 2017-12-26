9ee6bcfc0355c76beef27e359e8958c6d7f64f84
Jenny Lian <jennylian100@gmail.com>
2017-12-26 15:50:44 -0800

    alright finished

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,43 @@ use std::cmp::Ordering; // Ordering cmp
 use rand::Rng; // use you say use rand...
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the number! From a 1 - 100 inclusive");
 
     let secret_number = rand::thread_rng().gen_range(1, 101); // inclusive lower, exclusive high (1..100)
 
-    println!("The secret number is: {}", secret_number);
+//    println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess");
+    loop {
+        println!("Please input your guess");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    // Strong type.
+        // Strong type.
 
-    // we can shadow... we can re-use the guess name...c
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        // we can shadow... we can re-use the guess name...
+        // before we used expect("...") to print an error message
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("That wasn't a number, can you just enter a number?");
+                continue},
+        };
 
-    println!("You guesses: {}", guess);
+        println!("You guesses: {}", guess);
 
-    // Matching like Erlang?
-    // match is a statement
-    // Ordering is an emnum
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too much"),
-        Ordering::Equal => println!("Win!"),
+        // Matching like Erlang?
+        // match is a statement
+        // Ordering is an emnum
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too much"),
+            Ordering::Equal => {
+                println!("Win!");
+                break;
+            }
+        }
     }
 }
